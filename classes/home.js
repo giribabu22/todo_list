1f635b97e77775bf40417d4eebc39e2b502596ab
giribabu22 <giribabu22@navgurukul.org>
2022-11-30 08:16:15 +0530

    first_c

---
+++ classes/home.js
@@ -0,0 +1,22 @@
+const knex = require('../src/KnexCon')
+// const Knex = require('knex')
+
+class ResClass{
+    regis_POST(req,res) {
+        knex('register').insert(req.body).then((result) => {
+            res.json({ message: 'added successfully' })
+        }).catch((Errors) => {
+            res.json({ err: Errors })
+        });
+    }
+
+    create_do_POST(req, res) {
+        knex('does_table').insert(req.body).then((result) => {
+            res.redirect('/home')
+        }).catch((Errors) => {
+            res.json({err:Errors})
+        });
+    }
+}
+
+module.exports = ResClass
